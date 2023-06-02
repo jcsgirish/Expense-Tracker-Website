9d5190490fce5f9ca6ac811e11422336a430fdcd
jcsgirish <jcsgirish7@gmail.com>
2023-06-03 01:37:07 +0530

    Editing and Deleting Expenses

--- src/Components/Pages/Expenses.js
+++ src/Components/Pages/Expenses.js
@@ -1,16 +1,43 @@
-import React, { useContext, useRef, useEffect, useState } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import { expContext } from '../../Store/ExpenseContext';
-import { v4 as uuidv4 } from 'uuid';
 
 const Expenses = () => {
-  const [editFormOpen, setEditFormOpen] = useState(false);
+
   const ctx = useContext(expContext);
 
-  const handleDelete = async (expense) => {
-    console.log(expense);
+  useEffect(() => {
+    const fetchExpenses = async () => {
+      try {
+        const response = await fetch(
+          'https://expense-8a8d1-default-rtdb.firebaseio.com/expense.json'
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          const expenses = Object.keys(data).map((key) => ({
+            key: key,
+            ...data[key],
+          }));
+          ctx.setExpenses(expenses);
+        } else {
+          throw new Error('Failed to fetch expenses');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchExpenses();
+  }, []);
+
+
+
+  const deleteHandler = async (item) => {
     try {
-      let response = await fetch(
-        `https://expense-8a8d1-default-rtdb.firebaseio.com/expense/${expense.id}.json`,
+      console.log('Deleting item:', item);
+
+      const res = await fetch(
+        `https://expense-8a8d1-default-rtdb.firebaseio.com/expense/${item.key}.json`,
         {
           method: 'DELETE',
           headers: {
@@ -18,73 +45,66 @@ const Expenses = () => {
           },
         }
       );
-      if (response.ok) {
-        alert('Deleted successfully');
-        const newExpenses = ctx.expenses.filter((item) => item.id !== expense.id);
-        ctx.setExpenses(newExpenses);
+
+      console.log('Delete response:', res);
+
+      if (res.ok) {
+        alert('Expense Deleted Successfully');
+
+        const filteredExpenses = ctx.expenses.filter((expense) => expense.key !== item.key);
+ctx.setExpenses(filteredExpenses);
+        
       } else {
         throw new Error('Failed to delete expense');
       }
     } catch (error) {
-      console.log(error);
+      console.log(error.message);
     }
   };
 
-  const handleEdit = async (expense) => {
-    console.log(expense);
-    const editedExpense = {
-      expenseAmount: enteredAmount.current.value,
-      expenseDesc: enteredDesc.current.value,
-      expenseCategory: enteredCategory.current.value,
-    };
+  const editHandler = async (item) => {
     try {
-      let response = await fetch(
-        `https://expense-8a8d1-default-rtdb.firebaseio.com/expense/${expense.id}.json`,
-        {
-          method: 'PUT',
-          body: JSON.stringify(editedExpense),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-      if (response.ok) {
-        alert('Edited successfully');
-      } else {
-        throw new Error('Failed to edit expense');
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+      const updatedAmount = prompt('Enter updated amount:', item.expenseAmount);
+      const updatedDesc = prompt('Enter updated description:', item.expenseDesc);
+      const updatedCategory = prompt('Enter updated category:', item.expenseCategory);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let response = await fetch(
-          'https://expense-8a8d1-default-rtdb.firebaseio.com/expense.json',
+      if (updatedAmount && updatedDesc && updatedCategory) {
+        const updatedItem = {
+          ...item,
+          expenseAmount: updatedAmount,
+          expenseDesc: updatedDesc,
+          expenseCategory: updatedCategory,
+        };
+
+        const res = await fetch(
+          `https://expense-8a8d1-default-rtdb.firebaseio.com/expense/${item.key}.json`,
           {
-            method: 'GET',
+            method: 'PUT',
             headers: {
               'Content-Type': 'application/json',
             },
+            body: JSON.stringify(updatedItem),
           }
         );
-        if (response.ok) {
-          let data = await response.json();
-          console.log(data);
-          let finalData = Object.values(data || {});
-          ctx.setExpenses(finalData);
-        } else {
-          throw new Error('Failed to load previous expenses');
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
 
-    fetchData();
-  }, []);
+        if (res.ok) {
+          alert('Expense updated successfully');
+
+          const updatedExpenses = ctx.expenses.map((expense) =>
+          expense.key === item.key ? updatedItem : expense
+        );
+        ctx.setExpenses(updatedExpenses);
+          
+        } else {
+          throw new Error('Failed to update expense');
+        }
+      } else {
+        alert('Please enter valid values for all fields');
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   const enteredAmount = useRef();
   const enteredDesc = useRef();
@@ -94,18 +114,18 @@ const Expenses = () => {
     e.preventDefault();
 
     const newAddedExpense = {
-        id:uuidv4(),
       expenseAmount: enteredAmount.current.value,
       expenseDesc: enteredDesc.current.value,
       expenseCategory: enteredCategory.current.value,
     };
+
     if (
-      enteredAmount.current.value.length > 0 &&
-      enteredDesc.current.value.length > 0 &&
-      enteredCategory.current.value.length > 0
+      newAddedExpense.expenseAmount &&
+      newAddedExpense.expenseDesc &&
+      newAddedExpense.expenseCategory
     ) {
       try {
-        let response = await fetch(
+        const response = await fetch(
           'https://expense-8a8d1-default-rtdb.firebaseio.com/expense.json',
           {
             method: 'POST',
@@ -115,9 +135,15 @@ const Expenses = () => {
             },
           }
         );
+
         if (response.ok) {
+          const data = await response.json();
+          const newExpenseWithKey = {
+            ...newAddedExpense,
+            key: data.name,
+          };
           alert('Expense added successfully');
-          ctx.setExpenses([...ctx.expenses, newAddedExpense]);
+          ctx.setExpenses([...ctx.expenses, newExpenseWithKey]);
         } else {
           throw new Error('Failed to add expense');
         }
@@ -129,99 +155,101 @@ const Expenses = () => {
     }
   };
 
+ 
+
   return (
     <div className="container">
       <div className="row gx-3 gy-2 align-items-center mx-5 my-5">
         <h1>Enter your expense</h1>
         <form className="row gy-2 gx-3 align-items-center">
-          <div className="col-auto">
-            <label className="visually-hidden" htmlFor="expenseamount">
-              Expense amount:
-            </label>
-            <div className="input-group">
-              <div className="input-group-text"> Expense amount:</div>
-              <input
-                type="text"
-                className="form-control"
-                ref={enteredAmount}
-                id="expenseamount"
-              />
-            </div>
+        <div className="col-auto">
+          <label className="visually-hidden" htmlFor="expenseamount">
+            Expense amount:
+          </label>
+          <div className="input-group">
+            <div className="input-group-text"> Expense amount:</div>
+            <input
+              type="text"
+              className="form-control"
+              ref={enteredAmount}
+              id="expenseamount"
+            />
           </div>
-          <div className="col-auto">
-            <label className="visually-hidden" htmlFor="description">
-              Description:
-            </label>
-            <div className="input-group">
-              <div className="input-group-text">Description:</div>
-              <input
-                type="text"
-                className="form-control"
-                ref={enteredDesc}
-                id="description"
-              />
-            </div>
+        </div>
+        <div className="col-auto">
+          <label className="visually-hidden" htmlFor="description">
+            Description:
+          </label>
+          <div className="input-group">
+            <div className="input-group-text">Description:</div>
+            <input
+              type="text"
+              className="form-control"
+              ref={enteredDesc}
+              id="description"
+            />
           </div>
-          <div className="col-auto">
-            <label className="visually-hidden" htmlFor="category">
-              Category:
-            </label>
-            <div className="input-group">
-              <select
-                className="form-select"
-                defaultValue="choose a category"
-                ref={enteredCategory}
-                id="category"
-              >
-                <option value="choose a category" disabled>
-                  Category
-                </option>
-                <option value="movie">Movie</option>
-                <option value="food">Food</option>
-                <option value="electricity">Electricity</option>
-                <option value="fuel">Fuel</option>
-              </select>
-            </div>
-          </div>
-          <div className="col-auto"></div>
-          <div className="col-auto">
-            <button
-              type="submit"
-              className="btn btn-primary"
-              id="onSubmit"
-              onClick={handleSubmitExpense}
+        </div>
+        <div className="col-auto">
+          <label className="visually-hidden" htmlFor="category">
+            Category:
+          </label>
+          <div className="input-group">
+            <select
+              className="form-select"
+              defaultValue="choose a category"
+              ref={enteredCategory}
+              id="category"
             >
-              Submit
-            </button>
+              <option value="choose a category" disabled>
+                Category
+              </option>
+              <option value="movie">Movie</option>
+              <option value="food">Food</option>
+              <option value="electricity">Electricity</option>
+              <option value="fuel">Fuel</option>
+            </select>
           </div>
+        </div>
+        <div className="col-auto"></div>
+        <div className="col-auto">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            id="onSubmit"
+            onClick={handleSubmitExpense}
+          >
+            Submit
+          </button>
+        </div>
         </form>
       </div>
       {ctx.expenses.length > 0 && <h1 className="text-center">Expenses</h1>}
       <div className="row text-center ">
-        {ctx.expenses.map((expense) => {
+        {ctx.expenses.map((item) => {
           return (
-            <div key={expense.id} className="card w-25 col-4 mx-3 my-3">
+            <div key={item.key} className="card w-25 col-4 mx-3 my-3">
               <div className="card-body">
-                <h5 className="card-title">
-                  Amount:
-                  <span className="text-primary">{expense.expenseAmount}</span>
-                </h5>
-                <h5 className="card-title">
-                  Description:
-                  <span className="text-primary">{expense.expenseDesc}</span>
-                </h5>
-                <h5 className="card-title">
-                  Category:
-                  <span className="text-primary"> {expense.expenseCategory}</span>
-                </h5>
-                <span>
-                  <button className="btn btn-danger mx-1 my-1" onClick={() => handleDelete(expense)}>
-                    DELETE
-                  </button>
-                  <button className="btn btn-secondary mx-1 my-1" onClick={() => handleEdit(expense)}>
-                    EDIT
-                  </button>
-                </span>
+              <h5 className="card-title">
+                Amount:
+                <span className="text-primary">{item.expenseAmount}</span>
+              </h5>
+              <h5 className="card-title">
+                Description:
+                <span className="text-primary">{item.expenseDesc}</span>
+              </h5>
+              <h5 className="card-title">
+                Category:
+                <span className="text-primary"> {item.expenseCategory}</span>
+              </h5>
+              <span>
+                <button className="btn btn-danger mx-1 my-1" onClick={() => deleteHandler(item)}>
+                  DELETE
+                </button>
+                <button className="btn btn-secondary mx-1 my-1" onClick={() => editHandler(item)}>
+                  EDIT
+                </button>
+              </span>
               </div>
             </div>
           );
